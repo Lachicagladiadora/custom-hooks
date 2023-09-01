a7dbf1c366713bfdddc0b4fe8ada29c618665a18
Lachicagladiadora <200379@unsaac.edu.pe>
2023-08-31 19:24:00 -0500

    first custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 
+const useCounter = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increase = () => setCounter(counter+1)
+  const decrement = () => setCounter(counter+-1)
+  const reset = () => setCounter(0)
+
+  return{
+    counter, 
+    increase, 
+    decrement,
+    reset
+  }
+}
+
 export const App = () => {
-  const [count, setCount] = useState(0)
+  const {counter,increase, decrement, reset} = useCounter()
+  const counterB = useCounter()
 
   return (
     <>
-      <p>hello</p>
+     <h1>Custom Hooks</h1>
+     <div>
+      <div>{counter}</div>
+      <button onClick={reset}>zero</button>
+      <button onClick={increase}>+</button>
+      <button onClick={decrement}>-</button>
+     </div>
+     <div>
+      <div>{counterB.counter}</div>
+      <button onClick={counterB.increase}>+</button>
+     </div>
     </>
   )
 }
