a54796205e475096042db8577ef3a8c8fc459199
Lachicagladiadora <200379@unsaac.edu.pe>
2023-09-07 09:52:53 -0500

    passing event handlers to custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
+import { useState } from 'react'
 import './App.css'
 import { Counter } from './components/Counter'
 import { FormComponent } from './components/FormComponent'
 import { OnlineStatusBar } from './components/OnlineStatusBar'
 import { SaveButton } from './components/SaveButton'
 import { StatusBar } from './components/StatusBar'
-
+import { ChatRoom } from './components/ChatRoom'
 
 
 export const App = () => {
-  
+  const [roomId, setRoomId] = useState('general')
 
   return (
     <>
-       <h1>Custom Hooks</h1>
+      <h1>Custom Hooks</h1>
       <Counter/>
       <StatusBar/>
       <SaveButton/>
       <OnlineStatusBar/>
       <FormComponent/>
+      <h3>Passing event handlers to custom Hooks </h3>
+      <label>
+        Choose the chat room: {' '}
+        <select value={roomId} onChange={e=>setRoomId(e.target.value)}>
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+      </label>
+      <hr/>
+      <ChatRoom roomId={roomId}/>
     </>
   )
 }
---
+++ src/components/ChatRoom.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react"
+import Toastify from "toastify-js"
+
+const showNotification = (message: string) => {
+  Toastify({
+    text: message,
+    duration: 2000,
+    gravity: 'bottom',
+    position: 'right',
+    style:{
+      background: 'green',
+      color: 'white',
+      padding: '8px',
+      borderRadius: '3px',
+      height: '30px',
+      position: 'absolute',
+      bottom: '900px',
+      right: '10px',
+      zIndex:'1',
+      width: '600px' ,
+
+    }
+  }).showToast()
+}
+
+const createConnection = ({serverUrl,roomId}) => {
+  if(typeof serverUrl !== 'string'){
+    throw Error('Expected serverUrl to be a string. Received: ' + serverUrl)
+  }
+  if(typeof roomId !== 'string'){
+    throw Error('Expected roomId to be a string. Received: ' + roomId)
+  }
+  let intervalId: number | undefined
+  let messageCallback: ((arg0: string) => void) | null
+  return {
+    connect(){
+      console.log('✅ Connecting to "' + roomId + '" room at ' + serverUrl + '...')
+      clearInterval(intervalId)
+      intervalId = setInterval(()=>{
+        if(messageCallback){
+          if(Math.random() > 0.5) {
+            messageCallback('hey there, do you go ')
+          } else {
+            messageCallback('XD I will go...')
+          }
+        }
+      }, 3000)
+    },
+    disconnect(){
+      clearInterval(intervalId)
+      messageCallback = null
+      console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl + '')
+    },
+    onabort(event: string,callback: ((arg0: string) => void) | null){
+      if(messageCallback){
+      throw Error('Cannot add the handler twice.')
+      }
+      if(event !== 'message') {
+        throw Error('Only "message" event is supported.')
+      }
+      messageCallback = callback
+    }
+  }
+}
+
+export const ChatRoom = ({roomId}) => {
+  const [serverUrl, setServerUrl] = useState('https://localhost:1234')
+
+  useEffect(()=>{
+    const options = {
+      serverUrl: serverUrl,
+      roomId: roomId
+    }
+    const connection = createConnection(options)
+    connection.onabort('message', (msg: string)=>{
+      showNotification('New message: ' + msg)
+    })
+    connection.connect()
+    return () => connection.disconnect()
+  }, [roomId, serverUrl])
+  return(
+    <>
+      <label>
+        Server URL: 
+        <input value={serverUrl} onChange={e=>setServerUrl(e.target.value)}/>
+      </label>
+      <h2>Welcome to the {roomId} room!</h2>
+    </>
+  )
+}
