bc5afa788dcea04711d98444e2668b6e58407241
Lachicagladiadora <200379@unsaac.edu.pe>
2023-09-08 13:28:59 -0500

    Passing reactive values between Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import { FormComponent } from './components/FormComponent'
 import { OnlineStatusBar } from './components/OnlineStatusBar'
 import { SaveButton } from './components/SaveButton'
 import { StatusBar } from './components/StatusBar'
+import { ChatRoom } from './components/ChatRoom'
+import { useState } from 'react'
 
 
 export const App = () => {
+  const [roomId, setRoomId] = useState('general')
 
   return (
     <>
@@ -29,6 +32,26 @@ export const App = () => {
 
       <OnlineStatusBar/>
       <FormComponent/>
+
+      <div style={{
+        border: 'solid 2px green',
+        borderRadius: '10px',
+        padding: '10px',
+        background:'#143414a1',
+      }}>
+        <h3>Passing event handlers to custom Hooks </h3>
+        <label>
+          Choose the chat room: {' '}
+          <select value={roomId} onChange={e=>setRoomId(e.target.value)}>
+            <option value="general">general</option>
+            <option value="travel">travel</option>
+            <option value="music">music</option>
+          </select>
+        </label>
+        <hr/>
+        <ChatRoom roomId={roomId}/>
+        <small><strong>open inspect please</strong></small>
+      </div>
     </>
   )
 }
